31d0f69fbc7cafb502b213224aa9506372b0a6c2
Yisrael Dov Lebow <lebow@lebowtech.com>
2026-04-21 14:33:38 +0300

    set up direnv flake dev shell

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Dev shell for composer-subtree-plugin";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  };
+
+  outputs =
+    { nixpkgs, ... }:
+    let
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      devShells = forAllSystems (
+        system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              php
+              phpPackages.composer
+              gitFull
+              openssh
+              zip
+              unzip
+              cacert
+            ];
+          };
+        }
+      );
+    };
+}
